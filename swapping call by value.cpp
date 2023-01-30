b35a5dd8acaac57a0ea5c2d0835b49df6d6f7daf
SIDHARTH <83917260+SID9810@users.noreply.github.com>
2023-01-30 16:13:54 +0530

    Update swapping call by value.cpp

--- swapping call by value.cpp
+++ swapping call by value.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
-usng namespace std;
+using namespace std;
 int swap(int a, int b){
 	int temp=a;
-	a=b;
+	a=b;                      //swapping dosent take place 
+	                          //copied value agrument m jati h isliye nhi hogi swapping
 	b=temp;
 }
 int main(){
 	int a=3,b=4;
 	cout<<"the value of a is:"<<a<<endl;
 	cout<<"the value of b is:"<<b<<endl;
-	swap(int a, int b);
+	swap(a,b);
 	cout<<"the swaped value of a is:"<<a<<endl;
 	cout<<"the swapped value of b is:"<<b<<endl;
 	return 0;
